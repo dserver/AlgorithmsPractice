f30bb6ae2143ef9686572eabcabb9aeff1a1539d
dserver <jonathanhenk@gmail.com>
2015-12-30 14:52:23 -0500

    Add MaxPQ interface functions. All blank right now.

--- AlgorithmPractice/Problems/Sorting/HeapPQ/Solution/ArrayHeapPQ.cs
+++ AlgorithmPractice/Problems/Sorting/HeapPQ/Solution/ArrayHeapPQ.cs
@@ -9,4 +9,51 @@ namespace AlgorithmPractice.Problems.Sorting.HeapPQ.Solution
     class ArrayHeapPQ
     {
     }
+
+    class SimpleMaxPQ
+    {
+        private int max_capacity;
+
+        // create PQ with initial capacity max
+        public SimpleMaxPQ(int max)
+        {
+            max_capacity = max;
+        }
+
+        // create PQ from array A
+        public SimpleMaxPQ(int[] a)
+        {
+
+        }
+
+        // insert key into the PQ
+        public void insert(int key)
+        {
+
+        }
+
+        // return max item in PQ.
+        public int max()
+        {
+            return 0;
+        }
+
+        // return and remove largest item in PQ
+        public int delMax()
+        {
+            return 0;
+        }
+
+        // check if PQ has any items in it.
+        public bool isEmpty()
+        {
+            return true;
+        }
+
+        // return number of items in the PQ
+        public int size()
+        {
+            return 0;
+        }
+    }
 }
