2677ea9697e2d0310063baa1f15033f2cc738d2c
dserver <jonathanhenk@gmail.com>
2015-12-30 17:09:45 -0500

    Implemented constructors, insert, swim, swap, max, delMax (not sink yet. Haven't tested any methods yet

--- AlgorithmPractice/Problems/Sorting/HeapPQ/Solution/ArrayHeapPQ.cs
+++ AlgorithmPractice/Problems/Sorting/HeapPQ/Solution/ArrayHeapPQ.cs
@@ -13,35 +13,75 @@ namespace AlgorithmPractice.Problems.Sorting.HeapPQ.Solution
     class SimpleMaxPQ
     {
         private int max_capacity;
+        private List<int> pq;
+        private int insert_index = 1;
 
         // create PQ with initial capacity max
         public SimpleMaxPQ(int max)
         {
             max_capacity = max;
+            pq = new List<int>();
         }
 
         // create PQ from array A
         public SimpleMaxPQ(int[] a)
         {
-
+            pq = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+                this.insert(a[i]);
         }
 
         // insert key into the PQ
         public void insert(int key)
         {
+            pq[insert_index] = key;
+            swim(insert_index);
+            insert_index++;
+        }
+
+        // balance the heap from the child at position k
+        private void swim(int k)
+        {
+            int val = pq[k];
+            int parent_index = k / 2;
+            int parent = pq[parent_index];
+            while (k > 1 && pq[k / 2] < pq[k])
+            {
+                pqSwap(k / 2, k);
+                k = k / 2;
+            }
+
+            return;
+        }
+
+        private void pqSwap(int i, int j)
+        {
+            int temp = pq[i];
+            pq[i] = pq[j];
+            pq[j] = temp;
 
         }
 
         // return max item in PQ.
         public int max()
         {
-            return 0;
+            return pq[1];
         }
 
         // return and remove largest item in PQ
         public int delMax()
         {
-            return 0;
+            int r = pq[1];
+            pqSwap(insert_index, 1);
+            insert_index--;
+            pqSink(1);
+
+            return r;
+        }
+
+        public void pqSink(int k)
+        {
+
         }
 
         // check if PQ has any items in it.
