e697a5790a233672894e105f688cfa4ff88116e0
dserver <jonathanhenk@gmail.com>
2015-12-30 15:01:09 -0500

    added Insert method unit test

--- AlgorithmPractice/Problems/Sorting/HeapPQ/UnitTests/SimpleMaxPQTests.cs
+++ AlgorithmPractice/Problems/Sorting/HeapPQ/UnitTests/SimpleMaxPQTests.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using AlgorithmPractice.Problems.Sorting.HeapPQ.Solution;
 
 namespace AlgorithmPractice.Problems.Sorting.HeapPQ.UnitTests
 {
     class SimpleMaxPQTests
     {
+        class InsertMethod
+        {
+            [Test]
+            public void insert_5_items()
+            {
+                SimpleMaxPQ pq = new SimpleMaxPQ(10); // initial capacity of 10;
+                for (int i = 0; i < 5; i++)
+                    pq.insert(i); // add 0, 1, 2, 3, 4;
 
+                Assert.AreEqual(4, pq.max(), "A1");
+                Assert.AreEqual(5, pq.size(), "A2");
+            }
+        }
     }
 }
