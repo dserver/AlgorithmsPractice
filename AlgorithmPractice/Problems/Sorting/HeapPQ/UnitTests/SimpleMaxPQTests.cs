bb6dde6bf93944df0be58e5598d31fd6e94d6b50
dserver <jonathanhenk@gmail.com>
2015-12-31 20:07:13 -0500

    Added unit tests. 2 working tests right now for basic PQ creation

--- AlgorithmPractice/Problems/Sorting/HeapPQ/UnitTests/SimpleMaxPQTests.cs
+++ AlgorithmPractice/Problems/Sorting/HeapPQ/UnitTests/SimpleMaxPQTests.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using AlgorithmPractice.Problems.Sorting.HeapPQ.Solution;
 
 namespace AlgorithmPractice.Problems.Sorting.HeapPQ.UnitTests
 {
+    [TestFixture]
     class SimpleMaxPQTests
     {
+        [Test]
+        public void insertMethod_add5items_returnsNoExceptionThrown()
+        {
+            int[] a = { 5, 1, 3, 2, 4 };
+            SimpleMaxPQ m = new SimpleMaxPQ(a);
+            Assert.IsTrue(true, "Created object without issues");
+        }
 
+        [Test]
+        public void minMethod_add5ItemsGetMinEachTime_trueIfMinsAreRight()
+        {
+            int[] a = new int[5];
+            int c = 5;
+            for (int j = 0; j < a.Length; j++)
+                a[j] = c--;
+        }
     }
 }
