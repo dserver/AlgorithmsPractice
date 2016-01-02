28d26a0bfffe9c7808b13590caecfeba5c74bd17
dserver <jonathanhenk@gmail.com>
2016-01-02 12:05:56 -0500

    created interface that sorting algorithms can use. contains one method: sort

---
+++ AlgorithmPractice/Problems/Sorting/Interfaces/SortingAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice.Problems.Sorting.Interfaces
+{
+    interface SortingAlgorithm
+    {
+        public void sort(int[] a);
+    }
+}
