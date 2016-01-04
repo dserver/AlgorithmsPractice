5d5f29ee023634d31797abc27fe2a7f0786774b4
dserver <jonathanhenk@gmail.com>
2016-01-03 19:48:19 -0500

    working on mergesort solution based on notes in OneNote

--- AlgorithmPractice/Problems/Sorting/MergeSort/Solution/MergeSort.cs
+++ AlgorithmPractice/Problems/Sorting/MergeSort/Solution/MergeSort.cs
@@ -17,15 +17,21 @@ namespace AlgorithmPractice.Problems.Sorting.MergeSort.Solution
 
         public void merge(int[] a, int s1_low, int s1_high, int s2_low, int s2_high)
         {
+            for (int z = s1_low; z <= s2_high; z++)
+                aux[z] = a[z]; // copy to auxillary array
+
             int i = s1_low;
             int j = s2_low;
-
-            for (int z = s1_low; z < s2_high + 1; z++)
-                aux[z] = a[z];
-
-            for (; i <= s2_high; i++)
+            for (int k = s1_low; k <= s2_high; k++)
             {
-
+                if (i == s2_low)
+                    a[k] = aux[j++];
+                else if (j == s2_high + 1)
+                    a[k] = aux[i++];
+                else
+                {
+                    // to do
+                }
             }
         }
     }
